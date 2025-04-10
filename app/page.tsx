d88a4daa9ce63b95ce1918001e75c38674bd33c4
LagatUppsala <lagatuppsala@gmail.com>
2025-04-10 20:13:20 +0200

    init firebase

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,27 @@
+"use client"
+
+import { useEffect, useState } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../firebase/firebaseConfig";
+
+type Ingredient = {
+  embedding: number[];
+  name: string;
+};
+
+type Recipe = {
+  id: string;
+  name: string;
+  link_url: string;
+  ingredients: Ingredient[];
+};
 
 
 export default function Home() {
 
-
   return (
-    <div>
-      LAGAT
+    <div className="p-6">
+      <h1 className="text-3xl font-bold mb-6">LAGAT</h1>
     </div>
   );
 }
