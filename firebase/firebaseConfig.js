b7ed47215becebbf30b038d87cd617794b1168e4
LagatUppsala <lagatuppsala@gmail.com>
2025-04-11 20:17:40 +0200

    some debugging

--- firebase/firebaseConfig.js
+++ firebase/firebaseConfig.js
@@ -11,6 +11,9 @@ const firebaseConfig = {
     measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
+console.log("Firebase project ID:", process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID);
+
+
 
 const app = initializeApp(firebaseConfig)
 const db = getFirestore(app)
